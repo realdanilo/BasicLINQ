f98e7c348d387c8f854653037f119bea5d61607d
Danilo <realdanilo@users.noreply.github.com>
2020-12-30 22:24:32 -0700

    select, sort,

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -8,17 +8,27 @@ namespace LINQ
         static void Main(string[] args)
         {
             string sentence = "my name is danilo";
-            string[] names = { "danilo","dan","sharon","batman","rattatouie","francois","nicole","mike","jake","Yikes"};
+            string[] names = { "danilo","dan","sharon","batman","rattatouie","francois","nicole","mike","jake","Yikes", "yinyin","omg","Hello"};
             int[] numbers = { 12,4,3,1,11,8,99,0,3,2,2,6,19,23,83};
 
             var fivePlus = from number in numbers
-                           where number < 5
+                           where number < 5 || number >80
                            select number;
 
-            var
+            var fivePlusOrder = from number in numbers
+                                where number < 5 || number > 80
+                                orderby number descending
+                                select number;
+
             //Query > IEnumerable
             //Cannot access [#]
-            //Console.WriteLine(string.Join(" ",fivePlus));
+            //Runs only when is called
+
+            var namesWithA = from name in names
+                             where name.Contains("a") && name.Length <6
+                             select name;
+            
+            Console.WriteLine(string.Join(" ",fivePlusOrder));
         }
     }
 }
